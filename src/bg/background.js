a7be9a818b9036ce2e6f4c25df1180db07e10c89
Oscar Nordquist <on222bx@student.lnu.se>
2016-11-20 15:37:36 +0100

    Manifest & permission fixes

--- src/bg/background.js
+++ src/bg/background.js
@@ -8,6 +8,8 @@
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
+      chrome.storage.sync.set({'snowToggle': true}, function() {
+        // Notify that we saved.
+        sendResponse();
+      });
   });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,12 +2,9 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+		chrome.storage.sync.get("snowToggle", function (obj) {
+    		console.log(obj);
+		});
 	}
 	}, 10);
 });
