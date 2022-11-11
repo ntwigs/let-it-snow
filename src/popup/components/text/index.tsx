31edfbcb351e5866eb3287db1904c2cc4da84ece
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-11 10:40:17 +0100

    feat: add align to text

--- src/popup/components/text/index.tsx
+++ src/popup/components/text/index.tsx
@@ -1,12 +1,17 @@
-import styled, { DefaultTheme, ThemeProps } from 'styled-components'
+import styled, {
+  CSSProperties,
+  DefaultTheme,
+} from 'styled-components'
 
 type Props = {
   size?: keyof DefaultTheme['size']
   color?: keyof DefaultTheme['color']
+  align?: CSSProperties['textAlign']
 }
 
 export const Text = styled.p<Props>`
   font-family: 'Nunito', sans-serif;
   font-size: ${({ theme, size = 'medium' }) => theme.size[size]}px;
   color: ${({ theme, color = 'contrast' }) => theme.color[color]};
+  text-align: ${({ align = 'left' }) => align};
 `
