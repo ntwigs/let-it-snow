cb7cd8160ad279037b4cb755e6df137f2c188b4f
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-11 13:01:26 +0100

    feat: add warning icon

---
+++ src/popup/components/icon/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import warningIcon from '../../assets/warning-icon.svg'
+
+const Icon = styled.img`
+  width: ${({ theme }) => theme.getSize(8)}px;
+  height: ${({ theme }) => theme.getSize(8)}px;
+  aspect-ratio: 1;;
+`
+
+export const WarningIcon = () => <Icon src={warningIcon} alt="warning" />
--- src/popup/pages/unsupported/index.tsx
+++ src/popup/pages/unsupported/index.tsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 import { Header } from '../../blocks/header'
+import { Box } from '../../components/box'
 import { Container } from '../../components/container'
+import { WarningIcon } from '../../components/icon'
 import { Text } from '../../components/text'
 
 const Flex = styled.div`
@@ -8,13 +10,16 @@ const Flex = styled.div`
   justify-content: center;
   align-items: center;
   flex-grow: 1;
+  flex-direction: column;
 `
 export const Unsupported = (): JSX.Element => {
   return (
     <Container justifyContent="start">
       <Header title="LET IT SNOW" />
-      {/* <Icon type="warning" /> */}
       <Flex>
+        <Box bottom={2}>
+          <WarningIcon />
+        </Box>
         <Text align="center">This site is too warm for snow</Text>
       </Flex>
     </Container>
