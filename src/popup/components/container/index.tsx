0bf3c81165dc683751ed90774dfcb8532d44d4e0
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-11 10:39:32 +0100

    feat: add style prop to container

--- src/popup/components/container/index.tsx
+++ src/popup/components/container/index.tsx
@@ -1,9 +1,14 @@
-import styled from 'styled-components'
-export const Container = styled.main`
+import styled, { CSSProperties } from 'styled-components'
+
+type Props = {
+  justifyContent?: CSSProperties['justifyContent']
+}
+
+export const Container = styled.main<Props>`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+  justify-content: ${({ justifyContent = 'space-between' }) => justifyContent};
   width: ${({ theme }) => theme.dimensions.width}px;
   height: ${({ theme }) => theme.dimensions.height}px;
   background: ${({ theme }) => theme.color.primary};
