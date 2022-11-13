eeedb24158dc70e8c5ccd29c5e5651608b0a2bf8
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-13 19:33:32 +0100

    refactor: rename flex to center

--- src/popup/pages/unsupported/index.tsx
+++ src/popup/pages/unsupported/index.tsx
@@ -5,23 +5,24 @@ import { Container } from '../../components/container'
 import { WarningIcon } from '../../components/icon'
 import { Text } from '../../components/text'
 
-const Flex = styled.div`
+const Center = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-grow: 1;
   flex-direction: column;
 `
+
 export const Unsupported = (): JSX.Element => {
   return (
     <Container justifyContent="start">
       <Header title="LET IT SNOW" />
-      <Flex>
+      <Center>
         <Box bottom={2}>
           <WarningIcon />
         </Box>
         <Text align="center">This site is too warm for snow</Text>
-      </Flex>
+      </Center>
     </Container>
   )
 }
