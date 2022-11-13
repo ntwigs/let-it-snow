e891f1d076f85ef5478efedc6a6d4bf890c60ebe
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-13 19:33:02 +0100

    refactor: separate initial values hook

--- src/popup/pages/options/index.tsx
+++ src/popup/pages/options/index.tsx
@@ -1,7 +1,3 @@
-import { useMountEffect } from '@react-hookz/web'
-import { useState } from 'react'
-import { storageController } from '../../../storage'
-import { OptionValues } from '../../../storage/config'
 import { AmountSlider } from '../../blocks/amount-slider'
 import { Header } from '../../blocks/header'
 import { SizeSlider } from '../../blocks/size-slider'
@@ -9,48 +5,8 @@ import { SnowToggle } from '../../blocks/snow-toggle'
 import { SpeedSlider } from '../../blocks/speed-slider'
 import { Container } from '../../components/container'
 import { Unsupported } from '../unsupported'
+import { useInitialValues } from './use-initial-values'
 
-type DefaultData = {
-  amount: number
-  size: number
-  speed: number
-  isActive: boolean
-}
-
-const useInitialValues = () => {
-  const [isLoading, setLoading] = useState(true)
-  const [isError, setError] = useState(false)
-  const [initialValues, setInitialValues] = useState<OptionValues>(
-    {} as DefaultData
-  )
-
-  useMountEffect(() => {
-    const getValues = async () => {
-      try {
-        const storedValues = await storageController.getValues([
-          'amount',
-          'size',
-          'speed',
-          'isActive',
-        ])
-
-        setInitialValues(storedValues as OptionValues)
-      } catch {
-        setError(true)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    getValues()
-  })
-
-  return {
-    isLoading,
-    isError,
-    initialValues,
-  }
-}
 
 export const Options = (): JSX.Element | null => {
   const { isError, isLoading, initialValues } = useInitialValues()
---
+++ src/popup/pages/options/use-initial-values.ts
@@ -0,0 +1,39 @@
+import { useMountEffect } from '@react-hookz/web'
+import { useState } from 'react'
+import { storageController } from '../../../storage'
+import { OptionValues } from '../../../storage/config'
+
+export const useInitialValues = () => {
+  const [isLoading, setLoading] = useState(true)
+  const [isError, setError] = useState(false)
+  const [initialValues, setInitialValues] = useState<OptionValues>(
+    {} as OptionValues
+  )
+
+  useMountEffect(() => {
+    const getValues = async () => {
+      try {
+        const storedValues = await storageController.getValues([
+          'amount',
+          'size',
+          'speed',
+          'isActive',
+        ])
+
+        setInitialValues(storedValues as OptionValues)
+      } catch {
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    getValues()
+  })
+
+  return {
+    isLoading,
+    isError,
+    initialValues,
+  }
+}
