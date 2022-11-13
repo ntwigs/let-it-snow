170c16706a3d132d644c9b534a32df90cb0a7ff3
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-13 19:33:16 +0100

    feat: seperate support hook

--- src/popup/pages/start/index.tsx
+++ src/popup/pages/start/index.tsx
@@ -1,47 +1,8 @@
 import { Options } from '../options'
 import { Unsupported } from '../unsupported'
-import { useEffect, useState } from 'react'
-import { IS_SUPPORTED } from '../../../support/globals'
 import { Container } from '../../components/container'
+import { useSupport } from './use-support'
 
-type Support = {
-  isError: boolean
-  isLoading: boolean
-}
-const useSupport = (): Support => {
-  const [isLoading, setLoading] = useState<boolean>(true)
-  const [isError, setError] = useState<boolean>(false)
-
-  useEffect(() => {
-    const sendSupportCheck = async () => {
-      try {
-        const [tabData] = await chrome.tabs.query({
-          active: true,
-          currentWindow: true,
-        })
-
-        const result = await chrome.tabs.sendMessage(tabData.id!, {
-          message: IS_SUPPORTED,
-        })
-
-        if (!result) {
-          setError(true)
-        }
-      } catch {
-        setError(true)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    sendSupportCheck()
-  }, [])
-
-  return {
-    isLoading,
-    isError,
-  }
-}
 
 export const Start = () => {
   const { isLoading, isError } = useSupport()
---
+++ src/popup/pages/start/use-support.ts
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import { IS_SUPPORTED } from '../../../support/globals'
+
+type Support = {
+  isError: boolean
+  isLoading: boolean
+}
+export const useSupport = (): Support => {
+  const [isLoading, setLoading] = useState<boolean>(true)
+  const [isError, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    const sendSupportCheck = async () => {
+      try {
+        const [tabData] = await chrome.tabs.query({
+          active: true,
+          currentWindow: true,
+        })
+
+        const result = await chrome.tabs.sendMessage(tabData.id!, {
+          message: IS_SUPPORTED,
+        })
+
+        if (!result) {
+          setError(true)
+        }
+      } catch {
+        setError(true)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    sendSupportCheck()
+  }, [])
+
+  return {
+    isLoading,
+    isError,
+  }
+}
