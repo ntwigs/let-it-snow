b89118d99029c724d94b611786ec6f312dc57110
ntwigs <oscar.cv.nordquist@gmail.com>
2022-11-18 14:38:01 +0100

    style: update warning message style

--- src/popup/pages/unsupported/index.tsx
+++ src/popup/pages/unsupported/index.tsx
@@ -18,17 +18,24 @@ export const Unsupported = (): JSX.Element => {
     <Container justifyContent="start">
       <Header title="LET IT SNOW" />
       <Center>
-        <Box bottom={2}>
-          <WarningIcon />
+        <WarningIcon />
+        <Box x={2}>
+          <Box y={2}>
+            <Text align="center">This site is too hot for snow</Text>
+          </Box>
+          <Box bottom={2}>
+            <Text align="center" weight="medium" size="small">
+              Some sites are unsupported, like the{' '}
+              <Text as="span" weight="bold" size="small">
+                extension store
+              </Text>
+              .
+            </Text>
+          </Box>
+          <Text size="small" align="center" weight="medium">
+            If that’s not the issue - try reloading the page.
+          </Text>
         </Box>
-        <Text align="center">This site is too warm for snow</Text>
-        <Text align="center" weight="medium">
-          Some sites are unsupported, like the{' '}
-          <Text as="span" weight='bold'>extension store</Text>.
-        </Text>
-        <Text align="center" weight="medium">
-          If that’s not the issue - try reloading the page.
-        </Text>
       </Center>
     </Container>
   )
